eaaa11f485a09a0373c66c5f4b165f3ee6d9691e
Raúl López <sultanofwolves@gmail.com>
2022-08-04 11:22:23 +0200

    Add files via upload

---
+++ ft-channel/run.sh
@@ -0,0 +1,9 @@
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/standard-1.maude > ./results/standard-1.txt & disown
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/standard-2.maude > ./results/standard-2.txt & disown
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/standard-3.maude > ./results/standard-3.txt & disown
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/canonical-1.maude > ./results/canonical-1.txt & disown
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/canonical-2.maude > ./results/canonical-2.txt & disown
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/canonical-3.maude > ./results/canonical-3.txt & disown
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/filtered-1.maude > ./results/filtered-1.txt & disown
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/filtered-2.maude > ./results/filtered-2.txt & disown
+timeout 36000 maude ./../conditional-narrowing.maude ./ft-channel.maude ./tests/filtered-3.maude > ./results/filtered-3.txt & disown
